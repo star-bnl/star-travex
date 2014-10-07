e897eea4a938c563f339dcc62513588b8ac01e77
Dmitri Smirnov <d.s@plexoos.com>
2014-10-07 18:02:11 +0000

    Added ::GetCVS() method to our STAR makers

--- StiScan/StiScanHftTreeMaker.h
+++ StiScan/StiScanHftTreeMaker.h
@@ -10,6 +10,7 @@ public:
 
    StiScanHftTreeMaker(const Char_t *name = "StiScanHftTreeMaker");
    virtual Int_t Make();
+   virtual const char* GetCVS() const { static const char cvs[]="Tag $Name:  $ $Id: StiScanHftTreeMaker.h,v 1.2 2014/10/07 18:02:11 smirnovd Exp $ built " __DATE__ " " __TIME__; return cvs; }
 
 private:
 
