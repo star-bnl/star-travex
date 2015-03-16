ef44633338c5656df610dff202c65fdfa8a0e87d
Dmitri Smirnov <d.s@plexoos.com>
2015-03-16 16:12:52 +0000

    Initial commit of TStiEvent base class that can be used to save Sti related structures to root files

---
+++ StiRootIO/TStiEvent.cxx
@@ -0,0 +1,78 @@
+#include "StiRootIO/TStiEvent.h"
+#include "St_base/Stypes.h"
+
+ClassImp(TStiEvent);
+
+
+/// By default, we set no constraints on tracks w.r.t. their association with
+/// a specific detector group
+StDetectorId TStiEvent::fgDetGroupId = kMaxDetectorId;
+
+
+TStiEvent::TStiEvent() : TObject(), fTStiKalmanTracks(), fTStiHits()
+{
+}
+
+
+/**
+ * Constructs event in which track nodes will be constrained by the provided
+ * detector Id.
+ * */
+TStiEvent::TStiEvent(StDetectorId detGroupId) : TObject(), fTStiKalmanTracks(), fTStiHits()
+{
+   fgDetGroupId = detGroupId;
+}
+
+
+Int_t TStiEvent::Fill(StiTrackContainer &stiTrackContainer)
+{
+   for (auto trackIt = stiTrackContainer.begin(); trackIt != stiTrackContainer.end(); ++trackIt)
+   {
+      StiKalmanTrack* stiKTrack = static_cast<StiKalmanTrack*>(*trackIt);
+
+      if ( !stiKTrack ) {
+         Info("Fill", "Invalid kalman kTrack. Skipping to next one...");
+         continue;
+      }
+
+      if (fgDetGroupId == kMaxDetectorId)
+      {
+         // All tracks regardless of detector are accepted
+         fTStiKalmanTracks.push_back( TStiKalmanTrack(this, *stiKTrack) );
+         continue;
+
+      } else {
+         // Accept only tracks having a node associated with the given detector
+         for (StiKTNIterator it = stiKTrack->begin(); it != stiKTrack->end(); ++it)
+         {
+            StiKalmanTrackNode *stiNode = &(*it);
+            if ( !stiNode ) continue;
+
+            StDetectorId stiNodeDetId = stiNode->getDetector() ?
+               static_cast<StDetectorId>( stiNode->getDetector()->getGroupId() ) : kUnknownId;
+
+            if (stiNodeDetId == fgDetGroupId) {
+               fTStiKalmanTracks.push_back( TStiKalmanTrack(this, *stiKTrack) );
+               break;
+            }
+         }
+      }
+   }
+
+   return kStOK;
+}
+
+
+void TStiEvent::Print(Option_t *opt) const
+{
+   for (auto iTStiKTrack = fTStiKalmanTracks.begin(); iTStiKTrack != fTStiKalmanTracks.end(); ++iTStiKTrack) {
+      iTStiKTrack->Print();
+   }
+}
+
+
+void TStiEvent::Clear(Option_t *opt)
+{
+   fTStiKalmanTracks.clear();
+   fTStiHits.clear();
+}
---
+++ StiRootIO/TStiEvent.h
@@ -0,0 +1,39 @@
+#ifndef TStiEvent_h
+#define TStiEvent_h
+
+#include <vector>
+#include <set>
+#include <utility>
+
+#include "TObject.h"
+
+#include "StEvent/StEnumerations.h"
+#include "Sti/StiTrackContainer.h"
+#include "StiRootIO/TStiKalmanTrack.h"
+#include "StiRootIO/TStiHit.h"
+
+
+class TStiEvent : public TObject
+{
+public:
+
+   TStiEvent();
+   TStiEvent(StDetectorId detGroupId);
+
+   Int_t  Fill(StiTrackContainer &stiTrackContainer);
+   std::pair<std::set<TStiHit>::iterator, bool>   InsertStiHit(const TStiHit &stiHit) { return fTStiHits.insert(stiHit); }
+   const std::set<TStiHit>& GetStiHits() const { return fTStiHits; }
+   virtual void  Clear(Option_t *opt = "");
+   virtual void  Print(Option_t *opt = "") const;
+   const std::vector<TStiKalmanTrack>& GetTStiKalmanTracks() const { return fTStiKalmanTracks; }
+
+protected:
+
+   static StDetectorId           fgDetGroupId;        //!< Detector group id used in this study
+   std::vector<TStiKalmanTrack>  fTStiKalmanTracks;   ///< A collection of all Sti tracks of interest in this event
+   std::set<TStiHit>             fTStiHits;           ///< A collection of all Sti hits of interest in this event
+
+   ClassDef(TStiEvent, 1)
+};
+
+#endif
