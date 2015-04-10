da706ec319c401f6845564ef00874a9302bd5176
Dmitri Smirnov <d.s@plexoos.com>
2015-04-10 18:16:34 -0400

    Added cmake instructions to build StiScan and StiHify libraries

---
+++ StiScan/LinkDef.h
@@ -0,0 +1,13 @@
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ class H3D+;
+#pragma link C++ class Profile2D+;
+#pragma link C++ class Profile3D+;
+#pragma link C++ class StiScanEvent+;
+#pragma link C++ class StiScanHftTreeMaker+;
+
+#endif
