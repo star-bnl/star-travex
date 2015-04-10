63d35fa3333980ee62ae0e741925a485a0f4d994
Dmitri Smirnov <d.s@plexoos.com>
2015-04-10 17:35:13 -0400

    Added files for building the code with cmake

---
+++ StiRootIO/LinkDef.h
@@ -0,0 +1,13 @@
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ class StiTreeMaker+;
+#pragma link C++ class TStiEvent+;
+#pragma link C++ class TStiHit+;
+#pragma link C++ class TStiKalmanTrack+;
+#pragma link C++ class TStiKalmanTrackNode+;
+
+#endif
