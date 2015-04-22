73b0d8e898f292535c5210fe352a77c56e4e19c6
Dmitri Smirnov <d.s@plexoos.com>
2015-04-22 17:34:26 -0400

    StiHifyRootFile: Added hist containers and filled them with appropriate track node categories

--- StiHify/StiHifyRootFile.cxx
+++ StiHify/StiHifyRootFile.cxx
@@ -11,6 +11,7 @@
 
 #include "StiHify/StiHifyHistContainer.h"
 #include "StiScan/StiScanPrgOptions.h"
+#include "StiRootIO/TStiKalmanTrackNode.h"
 
 
 StiHifyRootFile::StiHifyRootFile(StiScanPrgOptions& prgOpts) : TFile(), mDirs(),
@@ -33,6 +34,9 @@ StiHifyRootFile::StiHifyRootFile(StiScanPrgOptions& prgOpts, const char *fname, 
 void StiHifyRootFile::BookHists()
 {
 
+   mDirs["sti_hit_closest"] = new StiHifyHistContainer(fPrgOptions, "sti_hit_closest", this);
+   mDirs["sti_hit_accepted"] = new StiHifyHistContainer(fPrgOptions, "sti_hit_accepted", this);
+   mDirs["sti_hit_rejected"] = new StiHifyHistContainer(fPrgOptions, "sti_hit_rejected", this);
 }
 
 
@@ -40,6 +44,14 @@ void StiHifyRootFile::FillHists(const StiHifyEvent &event, const std::set<std::s
 {
    StiHifyHistContainer* container;
 
+   container = static_cast<StiHifyHistContainer*> (mDirs["sti_hit_closest"]);
+   container->FillHists(event, StiNodeHitStatus::Any, volumeList);
+
+   container = static_cast<StiHifyHistContainer*> (mDirs["sti_hit_accepted"]);
+   container->FillHists(event, StiNodeHitStatus::Accepted, volumeList);
+
+   container = static_cast<StiHifyHistContainer*> (mDirs["sti_hit_rejected"]);
+   container->FillHists(event, StiNodeHitStatus::Rejected, volumeList);
 }
 
 
