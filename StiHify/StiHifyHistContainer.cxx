2766e61868539ed61d1c8565e697ba69ed4a3b16
Dmitri Smirnov <d.s@plexoos.com>
2015-04-23 12:21:24 -0400

    StiHifyHistContainer: Significantly reworked histogram filling containers routines

--- StiHify/StiHifyHistContainer.cxx
+++ StiHify/StiHifyHistContainer.cxx
@@ -45,14 +45,32 @@ void StiHifyHistContainer::BookHists()
 }
 
 
-void StiHifyHistContainer::FillHists(const StiHifyEvent &event, const std::set<std::string> *volumeList)
+void StiHifyHistContainer::FillHists(const StiHifyEvent &event, StiNodeHitStatus hitStatus, const std::set<std::string> *volumeList)
 {
-   std::vector<TStiKalmanTrack>::const_iterator iTStiKTrack = event.GetTStiKalmanTracks().begin();
-
-   for ( ; iTStiKTrack != event.GetTStiKalmanTracks().end(); ++iTStiKTrack)
+   for (auto iTStiKTrack=event.GetTStiKalmanTracks().begin(); iTStiKTrack!=event.GetTStiKalmanTracks().end(); ++iTStiKTrack)
    {
       const TStiKalmanTrack &kalmTrack = *iTStiKTrack;
-      FillHists(kalmTrack, volumeList);
+
+      for (auto iTStiKTrackNode=kalmTrack.GetNodes().begin(); iTStiKTrackNode!=kalmTrack.GetNodes().end(); ++iTStiKTrackNode)
+      {
+         const TStiKalmanTrackNode &trkNode = *iTStiKTrackNode;
+
+         switch (hitStatus)
+         {
+         case StiNodeHitStatus::Any:
+            FillHists(trkNode, volumeList);
+            break;
+         case StiNodeHitStatus::Accepted:
+            FillHistsHitsAccepted(trkNode, volumeList);
+            break;
+         case StiNodeHitStatus::Rejected:
+            FillHistsHitsRejected(trkNode, volumeList);
+            break;
+         default:
+            Error("FillHists", "Internal type of Sti hit assigned to this node is not specified. Histograms won't be filled");
+            break;
+         }
+      }
    }
 }
 
@@ -60,23 +78,51 @@ void StiHifyHistContainer::FillHists(const StiHifyEvent &event, const std::set<s
 void StiHifyHistContainer::FillDerivedHists()
 {
    this->cd();
-
-
-
-
-
-
-
-
 }
 
 
-void StiHifyHistContainer::FillHists(const TStiKalmanTrack &kalmTrack, const std::set<std::string> *volumeList)
+void StiHifyHistContainer::FillHists(const TStiKalmanTrackNode &trkNode, const std::set<std::string> *volumeList)
 {
+   if (volumeList && volumeList->size() && !trkNode.MatchedVolName(*volumeList) )
+      return;
+
+   if (!trkNode.IsInsideVolume())
+      return;
+
+   if (trkNode.GetHit()) {
+      hDistClosest2AcceptedHit->Fill( fabs(trkNode.CalcDistanceToHit() - trkNode.CalcDistanceToClosestHit()) );
+   }
+
+   hPullClosestHit1D->Fill(trkNode.CalcDistanceToClosestHit() < 0 ? -1 : (trkNode.CalcDistanceToClosestHit()/trkNode.GetTrackProjErr().Mag()) );
+
+   TVector3 pull = trkNode.CalcPullClosestHit();
+   hPullClosestHit2D->Fill(pull.Z(), pull.Y());
+
+   for (auto iCandHit=trkNode.GetAdjacentHits().begin(); iCandHit!=trkNode.GetAdjacentHits().end(); ++iCandHit)
+   {
+      TVector3 pull = trkNode.CalcPullToHit(**iCandHit);
+      hPullCandidateHits2D->Fill(pull.Z(), pull.Y());
+   }
+
+   hActiveLayerCounts->Fill(trkNode.GetPositionLocal().Z(), trkNode.GetPositionLocal().Y());
+}
 
 
+void StiHifyHistContainer::FillHistsHitsAccepted(const TStiKalmanTrackNode &trkNode, const std::set<std::string> *volumeList)
+{
+   if (!trkNode.GetHit())
+      return;
+
+   FillHists(trkNode, volumeList);
+}
 
 
+void StiHifyHistContainer::FillHistsHitsRejected(const TStiKalmanTrackNode &trkNode, const std::set<std::string> *volumeList)
+{
+   if (trkNode.GetHit())
+      return;
+
+   FillHists(trkNode, volumeList);
 }
 
 
--- StiHify/StiHifyHistContainer.h
+++ StiHify/StiHifyHistContainer.h
@@ -14,6 +14,7 @@
 #include "StiRootIO/Profile2D.h"
 #include "StiRootIO/Profile3D.h"
 #include "StiRootIO/TStiKalmanTrack.h"
+#include "StiRootIO/TStiKalmanTrackNode.h"
 
 typedef std::map<std::string, TH1*>                    HistMap;
 typedef std::map<std::string, TH1*>::iterator          HistMapIter;
@@ -28,14 +29,16 @@ public:
    StiHifyHistContainer(StiScanPrgOptions& prgOpts, const char* name, TDirectory* motherDir = 0, bool doProjection=true, Option_t* option = "");
    ~StiHifyHistContainer();
 
-   void FillHists(const StiHifyEvent &event, const std::set<std::string> *volumeList=0);
+   void FillHists(const StiHifyEvent &event, StiNodeHitStatus hitStatus=StiNodeHitStatus::Any, const std::set<std::string> *volumeList=0);
    void FillDerivedHists();
    void SaveAllAs(std::string prefix="./");
 
 protected:
 
    void BookHists();
-   virtual void FillHists(const TStiKalmanTrack &kalmTrack, const std::set<std::string> *volumeList=0);
+   void FillHists(const TStiKalmanTrackNode &trkNode, const std::set<std::string> *volumeList=0);
+   void FillHistsHitsAccepted(const TStiKalmanTrackNode &trkNode, const std::set<std::string> *volumeList=0);
+   void FillHistsHitsRejected(const TStiKalmanTrackNode &trkNode, const std::set<std::string> *volumeList=0);
 
    HistMap mHs;
 
