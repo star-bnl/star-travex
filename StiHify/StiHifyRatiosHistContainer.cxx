3e09c94cd3bed7c88a6b7d4cf9e0a646648852d4
Dmitri Smirnov <d.s@plexoos.com>
2015-08-13 18:22:47 -0400

    StiHifyRatiosHistContainer: Added 1D profiles from generated 2D ratio plots

--- StiHify/StiHifyRatiosHistContainer.cxx
+++ StiHify/StiHifyRatiosHistContainer.cxx
@@ -1,6 +1,7 @@
 #include "StiHify/StiHifyRatiosHistContainer.h"
 
 #include "TH1.h"
+#include "StiRootIO/Profile2D.h"
 
 
 StiHifyRatiosHistContainer::StiHifyRatiosHistContainer(const char* name, TDirectory* motherDir, Option_t* option) :
@@ -20,9 +21,20 @@ void StiHifyRatiosHistContainer::CreateRatioHist(const TH1* hNumer, const TH1* h
    this->cd();
 
    TH1 *myRatio = static_cast<TH1*>(hNumer->Clone());
-   myRatio->SetOption("colz");
 
+   myRatio->SetOption("colz");
+   myRatio->Sumw2();
    myRatio->Divide(hNumer, hDenom, 1, 1, "B");
 
    mHs[std::string(myRatio->GetName())] = myRatio;
+
+   // Create a 2D profile form the 2D ratio histogram
+   if (myRatio->GetDimension() == 2)
+   {
+      Profile2D* myRatioProfile = new Profile2D(static_cast<TH2D&>(*myRatio));
+      myRatioProfile->SetName( (std::string(myRatio->GetName()) + "_pxy").c_str() );
+
+      mHs[std::string(myRatioProfile->GetName())]          = myRatioProfile;
+      mHs[std::string(myRatioProfile->GetName()) + "_px"]  = myRatioProfile->ProfileX();
+   }
 }
