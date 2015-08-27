7efa6b22958f08d49b8739a3601e29d8e7cb352e
Dmitri Smirnov <d.s@plexoos.com>
2015-08-27 14:32:22 -0400

    web: Switched to new version (not backward compatible)

--- src-web/stihify/index.php
+++ src-web/stihify/index.php
@@ -10,10 +10,10 @@ include_once(BASE_DIR."/common/head.php");
 
 <h1>Stihify Results</h1>
 
-<h2 class="colsep">v0.1</h2>
+<h2 class="colsep">v0.2</h2>
 
 <ul>
-   <li><a href="plots.php?pfx=v0.1">TPC</a>
+   <li><a href="plots.php?pfx=v0.2">TPC</a>
 </ul>
 
 
