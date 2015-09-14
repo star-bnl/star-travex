75e5dcffb5f844e5be9632e35db541cdd0dbc4be
Dmitri Smirnov <d.s@plexoos.com>
2015-09-14 13:45:18 -0400

    VxEff: Changed VxEff() function signature
    Now we can accept any number of input files for histogram comparison on the same
    plot.

--- VxEff.C
+++ VxEff.C
@@ -1,25 +1,33 @@
 #include <string>
 #include <vector>
+#include <map>
 
 #include "TFile.h"
 #include "TCanvas.h"
 #include "TH1.h"
 #include "TLegend.h"
 #include "TEfficiency.h"
+#include "TStyle.h"
+
+#include "VxEff.h"
 
 
 TCanvas gVtxEffCanvas("vtx_eff", "Vertex Finding Efficiencies, Std and TMVA ranking", 800, 800);
 TLegend gVtxEffLegend(0.6, 0.30, 0.98, 0.50);
+
+const std::map<std::string, std::string> myDefaultVertexFiles = {
+   {"STD",  "mytestout.root"},
+   {"TMVA", "mytestout.root"}
+};
+
 
 /**
  * Creates and displays histograms with vertex finding efficiency as a function
  * of track multiplicity. The function takes two ROOT file names containing
  * histograms corresponging to standard and TMVA vertex ranks.
  */
-void VxEff(std::string fname_vtx_std_rank, std::string fname_vtx_tmva_rank)
+int VxEff(const std::map<std::string, std::string> & vtx_file_names)
 {
-   std::vector<std::string> fileNames = {fname_vtx_std_rank, fname_vtx_tmva_rank};
-   const char *M[2] = {"Std", "TMVA"};
 
    TH1F *frame = gVtxEffCanvas.DrawFrame(0, 0, 40, 1.1);
    frame->SetTitle("Vertex Finding Efficiencies");
@@ -27,8 +35,12 @@ void VxEff(std::string fname_vtx_std_rank, std::string fname_vtx_tmva_rank)
    frame->SetXTitle("Reconstractible multiplicity");
 
 
-   for (int j = 0; j < 2; j++) { // Std and TMVA
-      TFile file(fileNames[j].c_str());
+   int file_indx = 0;
+
+   for (const auto& key2FileName : vtx_file_names)
+   {
+      TFile file(key2FileName.second.c_str());
+
       TH1D *hMcRecMulT    = (TH1D *) file.Get("McRecMulT");
 
       if (! hMcRecMulT) continue;
@@ -38,22 +50,25 @@ void VxEff(std::string fname_vtx_std_rank, std::string fname_vtx_tmva_rank)
       TH1D *hMcRecMulGood = (TH1D *) file.Get("McRecMulGood"); double G = hMcRecMulGood->GetEntries();
       TH1D *hMcRecMulBad  = (TH1D *) file.Get("McRecMulBad");  double B = hMcRecMulBad->GetEntries();
 
-      if (j == 0) {
+      // Plot the overall efficiency only from the first file
+      if (file_indx == 0) {
          TEfficiency *eff_total = new TEfficiency(*hMcRecMulAny, *hMcRecMulT);
-         eff_total->SetMarkerColor(3 * j + 1);
+         eff_total->SetMarkerColor(3*file_indx + 1);
          eff_total->Draw("same");
          gVtxEffLegend.AddEntry(eff_total, Form("Overall Efficiency (a) = %4.2f", A / T));
       }
 
       TEfficiency *efficiency = new TEfficiency(*hMcRecMulGood, *hMcRecMulT);
-      efficiency->SetMarkerColor(3 * j + 2);
-      gVtxEffLegend.AddEntry(efficiency, Form("%s Efficiency (b) = %4.2f", M[j], G / T));
+      efficiency->SetMarkerColor(3*file_indx + 2);
+      gVtxEffLegend.AddEntry(efficiency, Form("%s Efficiency (b) = %4.2f", key2FileName.first.c_str(), G / T));
       efficiency->Draw("same");
 
       TEfficiency *impurity = new TEfficiency(*hMcRecMulBad, *hMcRecMulT);
-      impurity->SetMarkerColor(3 * j + 3);
-      gVtxEffLegend.AddEntry(impurity, Form("%s Impurity (c) = %4.2f", M[j], B / T));
+      impurity->SetMarkerColor(3*file_indx + 3);
+      gVtxEffLegend.AddEntry(impurity, Form("%s Impurity (c) = %4.2f", key2FileName.first.c_str(), B / T));
       impurity->Draw("same");
+
+      file_indx++;
    }
 
    gVtxEffLegend.Draw();
--- VxEff.h
+++ VxEff.h
@@ -1,6 +1,13 @@
 #ifndef VxEff_h
 #define VxEff_h
 
-void VxEff(std::string fname_vtx_std_rank, std::string fname_vtx_tmva_rank);
+#include <map>
+#include <string>
+
+// Because this file is processed by rootcint we cannot use nice C++11
+// initializatoin here, instead need to use extern
+extern const std::map<std::string, std::string> myDefaultVertexFiles;
+
+int VxEff(const std::map<std::string, std::string> & vtx_file_names=myDefaultVertexFiles);
 
 #endif
