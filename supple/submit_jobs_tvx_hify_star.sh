948e95ddb0d917513917ca8bda89bdc54bf53cf3
Dmitri Smirnov <d.s@plexoos.com>
2015-09-29 15:17:58 -0400

    Updated condor job submiting script
    Removed unused TVX_INSTALL_PREFIX
    Automatically form OUT_DIR

--- supple/submit_jobs_tvx_hify_star.sh
+++ supple/submit_jobs_tvx_hify_star.sh
@@ -8,23 +8,22 @@
 
 : ${TVX_SOURCE_DIR:=${HOME}/travex}
 : ${TVX_BUILD_DIR:=${TVX_SOURCE_DIR}/build2}
-: ${TVX_INSTALL_PREFIX:=${HOME}/tvx_install}
-: ${OUT_DIR:=${HOME}/tmp_hify_out}
+: ${TVX_OUT_PREFIX:=${TVX_BUILD_DIR}_out}
 
 
 echo "The following variables will be used:"
 echo -e "\t TVX_SOURCE_DIR:     $TVX_SOURCE_DIR"
 echo -e "\t TVX_BUILD_DIR:      $TVX_BUILD_DIR"
-echo -e "\t TVX_INSTALL_PREFIX: $TVX_INSTALL_PREFIX"
-echo -e "\t OUT_DIR:            $OUT_DIR"
 
 
 TVX_DEACT_DET_LAYER="pxl_1"
 export TVX_INSTALL_DIR="${TVX_INSTALL_PREFIX}_$TVX_DEACT_DET_LAYER"
 
+OUT_DIR="${TVX_OUT_PREFIX}_$TVX_DEACT_DET_LAYER"
+mkdir -p $OUT_DIR
+
 # After this 'trap' command print out all command before execution
 trap 'echo "$ $BASH_COMMAND"' DEBUG
-
 
 cd $TVX_BUILD_DIR && pwd
 cmake -D CMAKE_INSTALL_PREFIX=$TVX_INSTALL_DIR -D CMAKE_CXX_FLAGS="-m32" -D BOOST_ROOT=$OPTSTAR ${TVX_SOURCE_DIR}
@@ -33,7 +32,6 @@ make -j8 install
 cp $TVX_SOURCE_DIR/y2014a.root $TVX_INSTALL_DIR/
 cp $TVX_SOURCE_DIR/supple/run_tvx_hify_star.sh $TVX_INSTALL_DIR/
 mkdir -p /tmp/my_sums/
-mkdir -p $OUT_DIR
 
 echo
 echo "Submitting job for $TVX_DEACT_DET_LAYER"
