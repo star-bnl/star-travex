8eab165a63a7e9b43bfaf351e90b4b735e4b36b8
Dmitri Smirnov <d.s@plexoos.com>
2015-09-29 15:10:22 -0400

    web: Changed the index page to display list of all layers

--- src-web/stihify/index.php
+++ src-web/stihify/index.php
@@ -10,14 +10,31 @@ include_once(BASE_DIR."/common/head.php");
 
 <h1>Stihify Results</h1>
 
-<h2 class="colsep">v0.2</h2>
+
+<h2 class="colsep">v0.3</h2>
 
 <ul>
-   <li><a href="plots_tpc.php?pfx=v0.2">TPC</a>
-   <li><a href="plots.php?pfx=v0.2&det=sst">SST</a>
-   <li><a href="plots.php?pfx=v0.2&det=ist">IST</a>
-   <li><a href="plots.php?pfx=v0.2&det=pxl2">PXL2</a>
-   <li><a href="plots.php?pfx=v0.2&det=pxl1">PXL1</a>
+   <li>TPC layers:<br>
+
+<?php
+$layers = range(1, 45);
+
+foreach($layers as $layer) {
+
+	$layer_nice = sprintf("%02d", $layer);
+   print "<a href=\"plots.php?pfx=v0.3&det=tpc_$layer_nice\">layer $layer_nice</a>";
+	if ( !($layer % 13) )
+		print "<br>\n";
+	else
+		print " &mdash; ";
+}
+?>
+
+   <li><a href="plots.php?pfx=v0.3&det=sst_01">SST</a>
+   <li><a href="plots.php?pfx=v0.3&det=ist_01">IST</a>
+   <li><a href="plots.php?pfx=v0.3&det=pxl_02">PXL2</a>
+   <li><a href="plots.php?pfx=v0.3&det=pxl_01">PXL1</a>
+
 </ul>
 
 
