f6b94222e0fafb00b415242de2d20c2002f6e6e6
Dmitri Smirnov <d.s@plexoos.com>
2015-10-21 10:27:47 -0400

    Minor stylistic and non-essential fixes

--- supple/submit_jobs_embedding_jpsi.sh
+++ supple/submit_jobs_embedding_jpsi.sh
@@ -5,21 +5,22 @@ if ( $#argv != 1 ) then
   echo " Usage : $0 [FSET number, 109 or something like that]"
   echo ""
   echo ""
-  exit
+  exit 1
 endif
 
-set template = "/star/u/smirnovd/star-vertex-eval/supple/embed_template_P10ik.xml"
+
+set FSET = "$1"
+set TEMPLATE = "/star/u/smirnovd/star-vertex-eval/supple/embed_template_P10ik.xml"
 set INPUT_FILES = "/star/data20/embedding/reco/2010_zerobias/daq/st_zerobias_adc_11063039_raw_2590003.daq"
 set OUTPUT_DIR = "/star/u/smirnovd/bnl_me/public/jpsi_sim/"
 set JOB_MAX_EVENTS = 10
 set BFC_MIXER_MACRO = "/star/u/smirnovd/star-vertex-eval/ext/star-macros/macros/embedding/bfcMixer_Tpx.C"
 
+
 mkdir -p $OUTPUT_DIR/sums
 mkdir -p $OUTPUT_DIR/log
-
-set FSET = "$1"
 
 # Copy the bfcMixer script to the current directory so it can be used by the job
 cp $BFC_MIXER_MACRO ./
 
-star-submit-template -template ${template} -entities FSET=$FSET,INPUT_FILES=$INPUT_FILES,OUTPUT_DIR=$OUTPUT_DIR,JOB_MAX_EVENTS=$JOB_MAX_EVENTS
+star-submit-template -template ${TEMPLATE} -entities FSET=$FSET,INPUT_FILES=$INPUT_FILES,OUTPUT_DIR=$OUTPUT_DIR,JOB_MAX_EVENTS=$JOB_MAX_EVENTS
