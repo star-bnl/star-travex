3c65de268d105cb2d49d8cee2d803eb23fe5e481
Dmitri Smirnov <d.s@plexoos.com>
2016-01-25 17:15:41 -0500

    web-stihify: Updated index page with TOC
    - Split TOC section into two: v0.3 -> a and b
    - Added link to results from v0.4a

--- src-web/stihify/index.php
+++ src-web/stihify/index.php
@@ -11,7 +11,14 @@ include_once(BASE_DIR."/common/head.php");
 <h1>Stihify Results</h1>
 
 
-<h2 class="colsep">v0.3</h2>
+<h2 class="colsep">v0.4a</h2>
+
+<ul>
+   <li><a href="plots.php?pfx=v0.4a&det=sst">SST</a>
+</ul>
+
+
+<h2 class="colsep">v0.3b</h2>
 
 <ul>
    <li>TPC layers:<br>
@@ -21,19 +28,41 @@ $layers = range(1, 45);
 
 foreach($layers as $layer) {
 
-	$layer_nice = sprintf("%02d", $layer);
-   print "<a href=\"plots.php?pfx=v0.3&det=tpc_$layer_nice\">layer $layer_nice</a>";
-	if ( !($layer % 13) )
-		print "<br>\n";
-	else
-		print " &mdash; ";
+   $layer_nice = sprintf("%02d", $layer);
+   print "<a href=\"plots.php?pfx=v0.3b&det=tpc_$layer_nice\">layer $layer_nice</a>";
+   if ( !($layer % 13) )
+      print "<br>\n";
+   else
+      print " &mdash; ";
 }
 ?>
 
-   <li><a href="plots.php?pfx=v0.3&det=sst_01">SST</a>
-   <li><a href="plots.php?pfx=v0.3&det=ist_01">IST</a>
-   <li><a href="plots.php?pfx=v0.3&det=pxl_02">PXL2</a>
-   <li><a href="plots.php?pfx=v0.3&det=pxl_01">PXL1</a>
+</ul>
+
+
+<h2 class="colsep">v0.3a</h2>
+
+<ul>
+   <li>TPC layers:<br>
+
+<?php
+$layers = range(1, 45);
+
+foreach($layers as $layer) {
+
+   $layer_nice = sprintf("%02d", $layer);
+   print "<a href=\"plots.php?pfx=v0.3a&det=tpc_$layer_nice\">layer $layer_nice</a>";
+   if ( !($layer % 13) )
+      print "<br>\n";
+   else
+      print " &mdash; ";
+}
+?>
+
+   <li><a href="plots.php?pfx=v0.3a&det=sst_01">SST</a>
+   <li><a href="plots.php?pfx=v0.3a&det=ist_01">IST</a>
+   <li><a href="plots.php?pfx=v0.3a&det=pxl_02">PXL2</a>
+   <li><a href="plots.php?pfx=v0.3a&det=pxl_01">PXL1</a>
 
 </ul>
 
