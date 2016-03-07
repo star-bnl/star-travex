a931e4b7d5a38e808af9908b72b50cfe02d0e512
Dmitri Smirnov <d.s@plexoos.com>
2016-03-07 15:11:41 -0500

    PlotHelper: Whitespace changes

--- src-web/common/PlotHelper.php
+++ src-web/common/PlotHelper.php
@@ -13,31 +13,31 @@ class PlotHelper
    }
 
 
-	function fileExists($pName)
-	{
-	   $pi = pathinfo($pName);
-	   $imgSrc  = STISCAN_RESULTS_DIR."/{$pi['dirname']}/c_{$pi['basename']}.png";
-		return file_exists($imgSrc);
-	}
+   function fileExists($pName)
+   {
+      $pi = pathinfo($pName);
+      $imgSrc  = STISCAN_RESULTS_DIR."/{$pi['dirname']}/c_{$pi['basename']}.png";
+      return file_exists($imgSrc);
+   }
 
 
    function img($pName, $w=null, $href=null)
    {
-	   $pi = pathinfo($pName);
+      $pi = pathinfo($pName);
 
       $strWidth = empty($w) ? "width=300" : "width=$w";
-	   $imgSrc   = "{$this->img_base_dir}/{$pi['dirname']}/c_{$pi['basename']}.png";
-	   $imgHref  = empty($href) ? "{$this->img_base_dir}/{$pi['dirname']}/c_{$pi['basename']}.png" : $href;
+      $imgSrc   = "{$this->img_base_dir}/{$pi['dirname']}/c_{$pi['basename']}.png";
+      $imgHref  = empty($href) ? "{$this->img_base_dir}/{$pi['dirname']}/c_{$pi['basename']}.png" : $href;
 
-		return "<a href='$imgHref'><img $strWidth src='$imgSrc'></a>";
+      return "<a href='$imgHref'><img $strWidth src='$imgSrc'></a>";
    }
 
 
    function htmlLinkHistContents($pName)
    {
-	   $pi = pathinfo($pName);
+      $pi = pathinfo($pName);
 
-	   $href = "{$this->img_base_dir}/{$pName}";
+      $href = "{$this->img_base_dir}/{$pName}";
 
       return "<a href='$href'>{$pi['basename']}</a>";
    }
