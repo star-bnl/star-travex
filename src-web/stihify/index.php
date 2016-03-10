dd8032f02885986dc9bcae1700ff59cb9a2fbcd9
Dmitri Smirnov <d.s@plexoos.com>
2016-03-10 14:01:01 -0500

    web-stihify: Updated index page with more links to results

--- src-web/stihify/index.php
+++ src-web/stihify/index.php
@@ -11,6 +11,16 @@ include_once(BASE_DIR."/common/head.php");
 <h1>Stihify Results</h1>
 
 
+<h2 class="colsep">v0.4b</h2>
+
+<ul>
+   <li><a href="plots.php?pfx=v0.4b&det=sst">SST</a> - <a href="plots.php?pfx=v0.4b&det=sst_hft_hits">SST (tracks with HFT hits)</a>
+   <li><a href="plots.php?pfx=v0.4b&det=ist">IST</a> - <a href="plots.php?pfx=v0.4b&det=ist_hft_hits">IST (tracks with HFT hits)</a>
+   <li><a href="plots.php?pfx=v0.4b&det=pxl2">PXL2</a> - <a href="plots.php?pfx=v0.4b&det=pxl2_hft_hits">PXL2 (tracks with HFT hits)</a>
+   <li><a href="plots.php?pfx=v0.4b&det=pxl1">PXL1</a> - <a href="plots.php?pfx=v0.4b&det=pxl1_hft_hits">PXL1 (tracks with HFT hits)</a>
+</ul>
+
+
 <h2 class="colsep">v0.4a</h2>
 
 <ul>
