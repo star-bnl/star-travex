0d03d6b2bc115c2bc21f099b925b87765343678b
Dmitri Smirnov <d.s@plexoos.com>
2016-06-29 18:13:56 -0400

    star-hify: Many re-organizing changes to bash and condor scripts
    Just refactoring without changes in functionality...
    
    - Instructions/documentations updated
    - Minor changes in variable names
    - Additional print statements

--- supple/run_tvx_hify_star.sh
+++ supple/run_tvx_hify_star.sh
@@ -1,104 +1,111 @@
 #!/usr/bin/env bash
 #
-# It is recommended to examine and set the appropriate values for the script's
-# named arguments before running this script. Search for "NAMED ARGUMENTS" to
-# find their input values below.
+# This shell script is a wrapper around two commands used to produce results for
+# single hit efficiency measurements for a STAR detector layer. First, we
+# reconstruct events from a *.daq file and produce a root file with a TTree.
+# Second, we run `stihify` from `star-travex` over that TTree to produce the
+# final results and a ROOT file with histograms.
 #
-# With default values the script can be simply executed as:
+# Before running this script it is recommended to examine the script's named
+# arguments (see the "NAMED ARGUMENTS" section below) and modify their default
+# values as needed.
+#
+# Once the appropriate values are set the script can be simply executed with a
+# single argument specifying a STAR detector layer as:
 #
 # $ run_tvx_hify_star.sh pxl_1|pxl_2|ist_1|sst_1|tpc_#
 #
-# or, one can overwrite selected arguments as:
+# One can always overwrite all or some of the arguments in following way:
 #
-# $ TVX_BFC_INPUT_FILE=/path/to/my_file.daq TVX_INSTALL_DIR=/my/existing/dir \
+# $ INPUT_FILE=/path/to/my_file.daq STAR_TRAVEX_INSTALL_DIR=/my/existing/dir \
 #   run_tvx_hify_star.sh pxl_1|pxl_2|ist_1|sst_1|tpc_#
 #
 # For example:
 #
-# $ cd my_tvx_install/
-# $ TVX_INSTALL_DIR=./ run_tvx_hify_star.sh tpc_7
+# $ cd my_star_travex_install_dir/
+# $ STAR_TRAVEX_INSTALL_DIR=./ run_tvx_hify_star.sh tpc_7
 #
 
 
 # NAMED ARGUMENTS: Set default values for script variables
 
 # A flag to chose between two running modes
-: ${TVX_DEACTIVATE_DETECTOR:=true}
+: ${STAR_DEACTIVATE_DETECTOR:=true}
 
 # Path to input file or a list of files. XXX: Need to deal with relative paths
-: ${TVX_BFC_INPUT_FILE:=/scratch/smirnovd/public/random_tests/st_physics_15164004_raw_2000022.daq}
+: ${INPUT_FILE:=/scratch/smirnovd/public/random_tests/st_physics_15164004_raw_2000022.daq}
 
 # The maximum number of events to process (assuming we start from the first event)
-: ${TVX_BFC_NEVENTS:=1000000}
+: ${BFC_NEVENTS:=1000000}
 
-# These default BFC options should be good for reconstructing real data
-: ${TVX_BFC_OPTIONS:="P2014a mtd btof pxlHit istHit sstHit BEmcChkStat CorrX OSpaceZ2 OGridLeak3D -hitfilt StiHifyTreeMaker"}
+# These default BFC options should be enough to reconstruct real data
+: ${BFC_OPTIONS:="P2014a mtd btof pxlHit istHit sstHit BEmcChkStat CorrX OSpaceZ2 OGridLeak3D -hitfilt StiHifyTreeMaker"}
 
-# TVX_INSTALL_DIR is the directory where star-travex was installed
-: ${TVX_INSTALL_DIR:=${HOME}/travex/build_install} && TVX_INSTALL_DIR=`cd "$TVX_INSTALL_DIR"; pwd`
+# STAR_TRAVEX_INSTALL_DIR is the directory where star-travex was installed
+: ${STAR_TRAVEX_INSTALL_DIR:=${HOME}/star-travex/build_install} && STAR_TRAVEX_INSTALL_DIR=`cd "$STAR_TRAVEX_INSTALL_DIR"; pwd`
 
 # A path to the ROOT file with detector's TGeo geometry
-: ${TVX_STAR_GEO_FILE:=$TVX_INSTALL_DIR/y2014a.root}
+: ${STAR_GEOMETRY_FILE:=$STAR_TRAVEX_INSTALL_DIR/y2014a.root}
 
 
 # Validate user's input
-if [ -d $TVX_INSTALL_DIR ]; then cd $TVX_INSTALL_DIR && echo "$ pwd" && pwd;
+if [ -d $STAR_TRAVEX_INSTALL_DIR ]; then cd $STAR_TRAVEX_INSTALL_DIR && echo "$ pwd" && pwd;
 else
    echo "ERROR: Directory does not exist"
-   echo -e "\t TVX_INSTALL_DIR=$TVX_INSTALL_DIR"
+   echo -e "\t STAR_TRAVEX_INSTALL_DIR=$STAR_TRAVEX_INSTALL_DIR"
    exit 1
 fi
 
 
-if [ ! -e $TVX_STAR_GEO_FILE ]
+if [ ! -e $STAR_GEOMETRY_FILE ]
 then
    echo "ERROR: Geometry file does not exist"
-   echo -e "\t TVX_STAR_GEO_FILE=$TVX_STAR_GEO_FILE"
+   echo -e "\t STAR_GEOMETRY_FILE=$STAR_GEOMETRY_FILE"
    exit 1
 fi
 
 
 if [ -n "$1" ] && [[ $1 =~ ^([a-zA-Z]+)_([0-9]+)$ ]]
 then
-   TVX_DETECTOR=$1
-   TVX_DETECTOR_ID=$(echo $TVX_DETECTOR | sed 's/^\(.*\)_\([0-9]\+\)$/\1/')
-   TVX_DETECTOR_LAYER_ID=$(echo $TVX_DETECTOR | sed 's/^\(.*\)_\([0-9]\+\)$/\2/')
-   TVX_DETECTOR_LAYER_ID_PADDED=$(printf "%02d" $TVX_DETECTOR_LAYER_ID)
+   STAR_DETECTOR=$1
+   STAR_DETECTOR_ID=$(echo $STAR_DETECTOR | sed 's/^\(.*\)_\([0-9]\+\)$/\1/')
+   STAR_DETECTOR_LAYER_ID=$(echo $STAR_DETECTOR | sed 's/^\(.*\)_\([0-9]\+\)$/\2/')
+   STAR_DETECTOR_LAYER_ID_PADDED=$(printf "%02d" $STAR_DETECTOR_LAYER_ID)
 else
    echo "ERROR: First parameter must be set:"
    echo "$ ${0##*/} pxl_1|pxl_2|ist_1|sst_1|tpc_#"
    exit 1
 fi
 
-case $TVX_DETECTOR in
+case $STAR_DETECTOR in
 
    pxl_1)
-   TVX_VOLUME_REGEX='^.*IDSM_1/PXMO_1/PXLA_[\d]+/LADR_1/PXSI_[\d]+/PLAC_1.*$'
-   TVX_HIFY_OPTIONS="--z-min=-15 --z-max=15 --y-min=-2 --y-max=3"
+   STAR_VOLUME_REGEX='^.*IDSM_1/PXMO_1/PXLA_[\d]+/LADR_1/PXSI_[\d]+/PLAC_1.*$'
+   STAR_HIFY_OPTIONS="--z-min=-15 --z-max=15 --y-min=-2 --y-max=3"
    ;;
 
    pxl_2)
-   TVX_VOLUME_REGEX='^.*IDSM_1/PXMO_1/PXLA_[\d]+/LADR_[234]/PXSI_[\d]+/PLAC_1.*$'
-   TVX_HIFY_OPTIONS="--z-min=-15 --z-max=15 --y-min=0 --y-max=5"
+   STAR_VOLUME_REGEX='^.*IDSM_1/PXMO_1/PXLA_[\d]+/LADR_[234]/PXSI_[\d]+/PLAC_1.*$'
+   STAR_HIFY_OPTIONS="--z-min=-15 --z-max=15 --y-min=0 --y-max=5"
    ;;
 
    ist_1)
-   TVX_VOLUME_REGEX='^.*IDSM_1/IBMO_1/IBAM_[\d]+/IBLM_\d/IBSS_1.*$'
-   TVX_HIFY_OPTIONS="--z-min=-30 --z-max=30 --y-min=-2 --y-max=8"
+   STAR_VOLUME_REGEX='^.*IDSM_1/IBMO_1/IBAM_[\d]+/IBLM_\d/IBSS_1.*$'
+   STAR_HIFY_OPTIONS="--z-min=-30 --z-max=30 --y-min=-2 --y-max=8"
    ;;
 
    sst_1)
-   TVX_VOLUME_REGEX='^.*IDSM_1/SFMO_1/SFLM_[\d]+/SFSW_[\d]+/SFSL_1/SFSD_1.*$'
-   TVX_HIFY_OPTIONS="--z-min=-40 --z-max=40 --y-min=-3 --y-max=8"
+   STAR_VOLUME_REGEX='^.*IDSM_1/SFMO_1/SFLM_[\d]+/SFSW_[\d]+/SFSL_1/SFSD_1.*$'
+   STAR_HIFY_OPTIONS="--z-min=-40 --z-max=40 --y-min=-3 --y-max=8"
    ;;
 
    tpc_*)
-   TVX_VOLUME_REGEX='^.*Tpc/Padrow_'$TVX_DETECTOR_LAYER_ID'/Sector_[\d]+.*$'
-   TVX_HIFY_OPTIONS="--z-min=-250 --z-max=250 --y-min=-50 --y-max=50"
+   STAR_VOLUME_REGEX='^.*Tpc/Padrow_'$STAR_DETECTOR_LAYER_ID'/Sector_[\d]+.*$'
+   STAR_HIFY_OPTIONS="--z-min=-250 --z-max=250 --y-min=-50 --y-max=50"
    ;;
 
    *)
-   echo "ERROR: Unknown detector $TVX_DETECTOR specified"
+   echo "ERROR: Unknown detector $STAR_DETECTOR specified"
    exit 1
    ;;
 
@@ -106,27 +113,27 @@ esac
 
 
 echo -e "Named arguments and their values:"
-echo -e "\t TVX_DETECTOR: $TVX_DETECTOR"
-echo -e "\t TVX_DETECTOR_ID: $TVX_DETECTOR_ID"
-echo -e "\t TVX_DETECTOR_LAYER_ID: $TVX_DETECTOR_LAYER_ID (pretty: $TVX_DETECTOR_LAYER_ID_PADDED)"
-echo -e "\t TVX_VOLUME_REGEX: $TVX_VOLUME_REGEX"
-echo -e "\t TVX_BFC_NEVENTS: $TVX_BFC_NEVENTS"
-echo -e "\t TVX_BFC_OPTIONS: $TVX_BFC_OPTIONS"
-echo -e "\t TVX_DEACTIVATE_DETECTOR: $TVX_DEACTIVATE_DETECTOR"
-echo -e "\t TVX_BFC_INPUT_FILE: $TVX_BFC_INPUT_FILE"
-echo -e "\t TVX_INSTALL_DIR: $TVX_INSTALL_DIR"
-echo -e "\t TVX_STAR_GEO_FILE: $TVX_STAR_GEO_FILE"
+echo -e "\t STAR_DETECTOR:            $STAR_DETECTOR"
+echo -e "\t STAR_DETECTOR_ID:         $STAR_DETECTOR_ID"
+echo -e "\t STAR_DETECTOR_LAYER_ID:   $STAR_DETECTOR_LAYER_ID (pretty: $STAR_DETECTOR_LAYER_ID_PADDED)"
+echo -e "\t STAR_VOLUME_REGEX:        $STAR_VOLUME_REGEX"
+echo -e "\t BFC_NEVENTS:              $BFC_NEVENTS"
+echo -e "\t BFC_OPTIONS:              $BFC_OPTIONS"
+echo -e "\t STAR_DEACTIVATE_DETECTOR: $STAR_DEACTIVATE_DETECTOR"
+echo -e "\t INPUT_FILE:               $INPUT_FILE"
+echo -e "\t STAR_TRAVEX_INSTALL_DIR:  $STAR_TRAVEX_INSTALL_DIR"
+echo -e "\t STAR_GEOMETRY_FILE:       $STAR_GEOMETRY_FILE"
 
 
 # After this 'trap' command print out all command before execution
 trap 'echo "$ $BASH_COMMAND"' DEBUG
 
 # Create supplementary files in the output directory
-echo $TVX_VOLUME_REGEX > save_sti_detectors.txt
+echo $STAR_VOLUME_REGEX > save_sti_detectors.txt
 cat save_sti_detectors.txt
 
-if [ "$TVX_DEACTIVATE_DETECTOR" = true ] ; then
-   echo "Deactivated detector: $TVX_DETECTOR"
+if [ "$STAR_DEACTIVATE_DETECTOR" = true ] ; then
+   echo "Deactivated detector: $STAR_DETECTOR"
    cp save_sti_detectors.txt deactivate_sti_detectors.txt
    cat deactivate_sti_detectors.txt
 fi
@@ -134,14 +141,14 @@ fi
 pwd && ls -la .
 
 # Create file with a TTree
-root4star -q -b -l 'bfc.C('$TVX_BFC_NEVENTS', "'$TVX_BFC_OPTIONS'", "'$TVX_BFC_INPUT_FILE'")'
+root4star -q -b -l 'bfc.C('$BFC_NEVENTS', "'$BFC_OPTIONS'", "'$INPUT_FILE'")'
 
 # Create file with histograms
 which stihify
 
 # Do some clean up of the file name
-TVX_BFC_INPUT_FILE=`echo ${TVX_BFC_INPUT_FILE} | sed 's/^@//g'`
-TVX_BFC_INPUT_FILE=`echo ${TVX_BFC_INPUT_FILE} | sed 's/\.list//g'`
-TVX_BFC_INPUT_FILE=${TVX_BFC_INPUT_FILE##*/}
+INPUT_FILE=`echo ${INPUT_FILE} | sed 's/^@//g'`
+INPUT_FILE=`echo ${INPUT_FILE} | sed 's/\.list//g'`
+INPUT_FILE=${INPUT_FILE##*/}
 
-stihify $TVX_HIFY_OPTIONS -f ${TVX_BFC_INPUT_FILE%.*}.stihify.root -c -g -o deact_${TVX_DETECTOR_ID}_${TVX_DETECTOR_LAYER_ID_PADDED}/
+stihify $STAR_HIFY_OPTIONS -f ${INPUT_FILE%.*}.stihify.root -c -g -o deact_${STAR_DETECTOR_ID}_${STAR_DETECTOR_LAYER_ID_PADDED}/
--- supple/submit_jobs_tvx_hify_star.sh
+++ supple/submit_jobs_tvx_hify_star.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 #
+# Use this script to submit jobs to condor.
+#
 # Usage:
 #
 # $ submit_jobs_tvx_hify_star.sh
@@ -10,15 +12,21 @@
 
 # Set typical default values for script variables
 : ${JOB_INPUT_FILE:=/star/data99/reco/AuAu200/hijing_382/b0_20/minbias/y2014a/gheisha_on/pileup_eval_Oct05/rcf15005_10_10evts.event.root}
+
 : ${JOB_NEVENTS:=1000000}
-: ${TVX_DEACTIVATE_DETECTOR:=true}
-: ${TVX_SOURCE_DIR:=${HOME}/star-travex} && TVX_SOURCE_DIR=`cd "$TVX_SOURCE_DIR"; pwd`
-: ${TVX_INSTALL_DIR:=${HOME}/star-travex/build4condor_install} && TVX_INSTALL_DIR=`cd "$TVX_INSTALL_DIR"; pwd`
-: ${TVX_OUT_PREFIX:=/path/to/some_dir} && mkdir -p $TVX_OUT_PREFIX && TVX_OUT_PREFIX=`cd "$TVX_OUT_PREFIX"; pwd`
 
-#TVX_DETECTOR_LAYERS=("pxl_1" "pxl_2" "ist_1" "sst_1" "tpc_1" "tpc_2" "tpc_3" "tpc_4" "tpc_5" "tpc_6" "tpc_7" "tpc_8" "tpc_9" "tpc_10" "tpc_11" "tpc_12" "tpc_13" "tpc_14" "tpc_15" "tpc_16" "tpc_17" "tpc_18" "tpc_19" "tpc_20" "tpc_21" "tpc_22" "tpc_23" "tpc_24" "tpc_25" "tpc_26" "tpc_27" "tpc_28" "tpc_29" "tpc_30" "tpc_31" "tpc_32" "tpc_33" "tpc_34" "tpc_35" "tpc_36" "tpc_37" "tpc_38" "tpc_39" "tpc_40" "tpc_41" "tpc_42" "tpc_43" "tpc_44" "tpc_45")
+# A flag to chose between two running modes
+: ${STAR_DEACTIVATE_DETECTOR:=true}
 
+: ${SOURCE_DIR:=${HOME}/star-travex} && SOURCE_DIR=`cd "$SOURCE_DIR"; pwd`
 
+# STAR_TRAVEX_INSTALL_DIR is the directory where star-travex was installed
+: ${STAR_TRAVEX_INSTALL_DIR:=${HOME}/star-travex/build_install} && STAR_TRAVEX_INSTALL_DIR=`cd "$STAR_TRAVEX_INSTALL_DIR"; pwd`
+
+: ${OUT_DIR:=/path/to/some_dir} && mkdir -p $OUT_DIR && OUT_DIR=`cd "$OUT_DIR"; pwd`
+
+#STAR_DETECTOR_LAYERS=("pxl_1" "pxl_2" "ist_1" "sst_1" "tpc_1" "tpc_2" "tpc_3" "tpc_4" "tpc_5" "tpc_6" "tpc_7" "tpc_8" "tpc_9" "tpc_10" "tpc_11" "tpc_12" "tpc_13" "tpc_14" "tpc_15" "tpc_16" "tpc_17" "tpc_18" "tpc_19" "tpc_20" "tpc_21" "tpc_22" "tpc_23" "tpc_24" "tpc_25" "tpc_26" "tpc_27" "tpc_28" "tpc_29" "tpc_30" "tpc_31" "tpc_32" "tpc_33" "tpc_34" "tpc_35" "tpc_36" "tpc_37" "tpc_38" "tpc_39" "tpc_40" "tpc_41" "tpc_42" "tpc_43" "tpc_44" "tpc_45")
+STAR_DETECTOR_LAYERS=("ist_1")
 
 # BFC options for simulation
 # We use chain options similar to those used for 2014 MC reconstruction
@@ -31,41 +39,43 @@ JOB_BFC_OPTIONS="in y2014a AgML pxlFastSim istFastSim usexgeom FieldOn MakeEvent
 # BFC options for real data
 #JOB_BFC_OPTIONS="P2014a mtd btof pxlHit istHit sstHit BEmcChkStat CorrX OSpaceZ2 OGridLeak3D -hitfilt StiHifyTreeMaker"
 
-
 # Replace all spaces with underscores. This is needed to pass the list of
 # options to the xml job template. Will not work with options containing a _
 JOB_BFC_OPTIONS=`echo ${JOB_BFC_OPTIONS[*]} | sed 's/ /_/g'`
 
 
-echo "The following variables will be used:"
-echo -e "\t TVX_SOURCE_DIR:         $TVX_SOURCE_DIR"
-echo -e "\t TVX_INSTALL_DIR:        $TVX_INSTALL_DIR"
-echo -e "\t TVX_OUT_PREFIX:         $TVX_OUT_PREFIX"
-echo -e "\t TVX_DETECTOR_LAYERS:    ${TVX_DETECTOR_LAYERS[*]}"
-echo -e "\t JOB_BFC_OPTIONS:        $JOB_BFC_OPTIONS"
+echo -e "The following variables are set:\n"
+echo -e "\t JOB_INPUT_FILE:           $JOB_INPUT_FILE"
+echo -e "\t JOB_NEVENTS:              $JOB_NEVENTS"
+echo -e "\t SOURCE_DIR:               $SOURCE_DIR"
+echo -e "\t STAR_TRAVEX_INSTALL_DIR:  $STAR_TRAVEX_INSTALL_DIR"
+echo -e "\t OUT_DIR:                  $OUT_DIR"
+echo -e "\t STAR_DETECTOR_LAYERS:     ${STAR_DETECTOR_LAYERS[*]}"
+echo -e "\t JOB_BFC_OPTIONS:          $JOB_BFC_OPTIONS"
 
 
 # After this 'trap' command print out all command before execution
 trap 'echo "$ $BASH_COMMAND"' DEBUG
 
-export TVX_INSTALL_DIR
-mkdir -p $TVX_OUT_PREFIX/sums/
-cd $TVX_OUT_PREFIX
+# Export STAR_TRAVEX_INSTALL_DIR so we can use it in .xml template
+export STAR_TRAVEX_INSTALL_DIR
+mkdir -p $OUT_DIR/sums/
+cd $OUT_DIR
 
 
-cp $TVX_SOURCE_DIR/y2014a.root $TVX_INSTALL_DIR/
-cp $TVX_SOURCE_DIR/supple/run_tvx_hify_star.sh $TVX_INSTALL_DIR/
+cp $SOURCE_DIR/y2014a.root $STAR_TRAVEX_INSTALL_DIR/
+cp $SOURCE_DIR/supple/run_tvx_hify_star.sh $STAR_TRAVEX_INSTALL_DIR/
 
 
-for TVX_DETECTOR in "${TVX_DETECTOR_LAYERS[@]}"
+for STAR_DETECTOR in "${STAR_DETECTOR_LAYERS[@]}"
 do
 
-   OUT_DIR="${TVX_OUT_PREFIX}/$TVX_DETECTOR"
+   OUT_DIR="${OUT_DIR}/$STAR_DETECTOR"
    mkdir -p $OUT_DIR
 
    echo
-   echo "Submitting job for $TVX_DETECTOR"
-   star-submit-template -template $TVX_SOURCE_DIR/supple/job_template_tvx_hify_star.xml \
-      -entities JOB_INPUT_FILE=$JOB_INPUT_FILE,JOB_NEVENTS=$JOB_NEVENTS,JOB_BFC_OPTIONS=$JOB_BFC_OPTIONS,OUT_DIR=$OUT_DIR,TVX_DETECTOR=$TVX_DETECTOR,TVX_DEACTIVATE_DETECTOR=$TVX_DEACTIVATE_DETECTOR
+   echo "Submitting job for $STAR_DETECTOR"
+   star-submit-template -template $SOURCE_DIR/supple/job_template_tvx_hify_star.xml \
+      -entities JOB_INPUT_FILE=$JOB_INPUT_FILE,JOB_NEVENTS=$JOB_NEVENTS,JOB_BFC_OPTIONS=$JOB_BFC_OPTIONS,OUT_DIR=$OUT_DIR,STAR_DETECTOR=$STAR_DETECTOR,STAR_DEACTIVATE_DETECTOR=$STAR_DEACTIVATE_DETECTOR
    echo
 done
