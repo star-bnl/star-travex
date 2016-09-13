863c65def7fbf1415bcc8f73fb5e5eeaec675690
Dmitri Smirnov <d.s@plexoos.com>
2016-09-13 12:32:44 -0400

    utils.h: Removed outdated code

--- src-tools/utils.h
+++ src-tools/utils.h
@@ -17,35 +17,7 @@ using VectorMcTracks = std::vector<const StMuMcTrack*>;
 vtxeval::VectorMcTracks getMcTracksMatchingMcVertex(const StMuDst& stMuDst, const StMuMcVertex* mcVertex);
 
 
-
 static bool gDebugFlag = false;
-
-/** Get input from user to control amount of print out interactively. */
-inline bool ask_user()
-{
-   static bool fAsk = true;
-   char symbol;
-
-   if (fAsk) {
-      std::cout << "ask (enter - next, r - don't ask, q - quit) >";
-
-      do {
-         std::cin.get(symbol);
-
-         if (symbol == 'r') {
-            gDebugFlag = false;
-            fAsk = false;
-         }
-         else if (symbol == 'q') return true;
-      }
-      while (symbol != '\n');
-
-      std::cout << std::endl;
-   }
-
-   return false;
-}
-
 
 }
 
