ce4a8a0bbb4d76490292679edaddd2234366d643
Dmitri Smirnov <d.s@plexoos.com>
2016-09-19 17:49:59 -0400

    Added new containers for decay vertex histograms

---
+++ VtxEval/DecayVertexHists.h
@@ -0,0 +1,21 @@
+#ifndef DecayVertexHists_h
+#define DecayVertexHists_h
+
+#include "travex/HistContainer.h"
+#include "StVertexRootIO/TDecayVertex.h"
+
+class StMuPrimaryVertex;
+class StMuMcVertex;
+
+
+class DecayVertexHists : public tvx::HistContainer
+{
+public:
+
+   DecayVertexHists(const std::string name, TDirectory* motherDir=nullptr, const std::string option="");
+
+   void FillHists(const StMuPrimaryVertex &vertex, const std::vector<TDecayVertex>& decayVertices);
+
+};
+
+#endif
