21fa3a9728c9ae1352fb0528908db22e7814dcee
Dmitri Smirnov <d.s@plexoos.com>
2017-02-24 15:01:27 -0500

    New ProgramOptions for vtxreco

---
+++ VtxEval/VtxRecoProgramOptions.cxx
@@ -0,0 +1,63 @@
+#include <iostream>
+
+#include <boost/program_options/errors.hpp>
+#include <boost/program_options/options_description.hpp>
+#include <boost/program_options/variables_map.hpp>
+
+#include "travex/utils.h"
+#include "VtxEval/VtxRecoProgramOptions.h"
+
+// A shorthand for a long namespace
+namespace po = boost::program_options;
+
+using VertexFit_t = star_vertex::VertexFit_t;
+using SeedFinder_t = star_vertex::SeedFinder_t;
+
+
+VtxRecoProgramOptions::VtxRecoProgramOptions(int argc, char **argv, const std::string& out_sfx, const std::string& out_ext) :
+   tvx::ProgramOptions(argc, argv, out_sfx, out_ext),
+   fVertexFit(VertexFit_t::Unspecified),
+   fSeedFinder(SeedFinder_t::Unspecified)
+{
+   // Declare supported options
+   fOptions.add_options()
+      ("vertex-fit,f",
+          po::value<VertexFit_t>(&fVertexFit)->default_value(VertexFit_t::Beamline3D),
+          "Type of vertex fit")
+
+      ("vertex-seed-finder,v",
+          po::value<SeedFinder_t>(&fSeedFinder)->default_value(SeedFinder_t::PPVLikelihood),
+          "Type of vertex seeding algorithm")
+   ;
+}
+
+
+
+void VtxRecoProgramOptions::VerifyOptions()
+{
+   // First check the parent class' options
+   tvx::ProgramOptions::VerifyOptions();
+
+
+   if (fOptionsValues.count("vertex-fit") && fVertexFit == VertexFit_t::Unspecified)
+   {
+      throw po::validation_error(po::validation_error::invalid_option_value, "", "vertex-fit");
+   }
+
+
+   if (fOptionsValues.count("vertex-seed-finder") &&
+       fSeedFinder == SeedFinder_t::Unspecified )
+   {
+      throw po::validation_error(po::validation_error::invalid_option_value, "", "vertex-seed-finder");
+   }
+}
+
+
+/** Prints the options and their values. */
+void VtxRecoProgramOptions::Print() const
+{
+   ProgramOptions::Print();
+
+   std::cout << "\tvertex-fit:\t" << fVertexFit << "\n";
+   std::cout << "\tvertex-seed-finder:\t" << fSeedFinder << "\n";
+}
---
+++ VtxEval/VtxRecoProgramOptions.h
@@ -0,0 +1,39 @@
+#ifndef VtxRecoProgramOptions_h
+#define VtxRecoProgramOptions_h
+
+#include "StGenericVertexMaker/VertexFinderOptions.h"
+
+#include "travex/ProgramOptions.h"
+
+
+
+/**
+ * Processes and controls user options provided in the command line.
+ */
+class VtxRecoProgramOptions : public tvx::ProgramOptions
+{
+
+   using VertexFit_t = star_vertex::VertexFit_t;
+   using SeedFinder_t = star_vertex::SeedFinder_t;
+
+public:
+
+   VtxRecoProgramOptions(int argc, char **argv, const std::string& out_sfx="vtxreco", const std::string& out_ext="MuDst.root");
+
+   void Print() const;
+
+   VertexFit_t  GetVertexFit() { return fVertexFit; }
+
+   SeedFinder_t  GetSeedFinder() { return fSeedFinder; }
+
+protected:
+
+   virtual void VerifyOptions();
+
+   VertexFit_t   fVertexFit;
+
+   SeedFinder_t  fSeedFinder;
+
+};
+
+#endif
