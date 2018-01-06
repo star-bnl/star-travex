d486de7b1b1e2c44ec094a9613921b54f5ac968b
Dmitri Smirnov <d.s@plexoos.com>
2018-01-06 10:25:56 -0500

    Moved URL param parsing to common

--- common/common.js
+++ common/common.js
@@ -2,3 +2,19 @@ $(function(){
   $("#header").load("../common/header.html"); 
   $("#footer").load("../common/footer.html"); 
 });
+
+
+var urlParams;
+
+(window.onpopstate = function () {
+    var match,
+        pl     = /\+/g,  // Regex for replacing addition symbol with a space
+        search = /([^&=]+)=?([^&]*)/g,
+        decode = function (s) { return decodeURIComponent(s.replace(pl, " ")); },
+        query  = window.location.search.substring(1);
+
+    urlParams = {};
+    while (match = search.exec(query))
+       urlParams[decode(match[1])] = decode(match[2]);
+
+})();
--- common/gdriveImage.js
+++ common/gdriveImage.js
@@ -9,22 +9,6 @@ var updateImages = function() {
 };
 
 
-var urlParams;
-
-(window.onpopstate = function () {
-    var match,
-        pl     = /\+/g,  // Regex for replacing addition symbol with a space
-        search = /([^&=]+)=?([^&]*)/g,
-        decode = function (s) { return decodeURIComponent(s.replace(pl, " ")); },
-        query  = window.location.search.substring(1);
-
-    urlParams = {};
-    while (match = search.exec(query))
-       urlParams[decode(match[1])] = decode(match[2]);
-
-})();
-
-
 function updateGDriveImages(urlParams)
 {
   // Adding the script tag to the head
