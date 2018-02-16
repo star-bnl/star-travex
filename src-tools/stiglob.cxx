4ce70d8160402d1181965f54f1fe90b0b0f9665a
Dmitri Smirnov <d.s@plexoos.com>
2018-02-16 15:15:31 -0500

    Add dummy definitions to resolve some unused global pointers

---
+++ src-tools/stiglob.cxx
@@ -0,0 +1,7 @@
+class StSstDbMaker;
+class StSsdDbMaker;
+class StSvtDbMaker;
+
+StSsdDbMaker *gStSsdDbMaker = nullptr;
+StSstDbMaker *gStSstDbMaker = nullptr;
+StSvtDbMaker *gStSvtDbMaker = nullptr;
