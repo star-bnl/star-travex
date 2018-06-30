4285db7346cb9719dea9e0afd6ed81793ee39e63
Dmitri Smirnov <d.s@plexoos.com>
2018-06-29 21:52:42 -0400

    viewTGeo.C: Rotate camera view and clip away 1/8

--- src/geo/viewTGeo.C
+++ src/geo/viewTGeo.C
@@ -8,11 +8,17 @@ void viewTGeo(std::string tgeoFileName = "tgeo_geom.root")
  
    TGLViewer* viewer = gEve->GetDefaultGLViewer();
    TGeoManager* tgeo = gEve->GetDefaultGeometry();
+
+   TGLPerspectiveCamera& cam = (TGLPerspectiveCamera&) viewer->CurrentCamera();
+   double center[3]= {0, 400, 0};
+   cam.Configure(30, -100, center, -0.3, -2.4);
+   viewer->SetGuideState(TGLUtil::kAxesOrigin, true, false, 0);
+   viewer->GetClipSet()->SetClipType(TGLClip::kClipBox);
  
    // Get the specified top volume and node to draw
    TGeoNode *topNode = tgeo->GetTopNode();
    TEveGeoTopNode *eveTopNode = new TEveGeoTopNode(tgeo, topNode);
  
    gEve->AddGlobalElement(eveTopNode);
-   gEve->Redraw3D(true);
+   gEve->Redraw3D();
 }
