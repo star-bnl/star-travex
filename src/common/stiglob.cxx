e5c8d786441339727a6b4051057dd328d45f2ae9
Dmitri Smirnov <d.s@plexoos.com>
2018-07-16 18:08:07 -0400

    Define variable used in STAR code but removed in ROOT6

--- src/common/stiglob.cxx
+++ src/common/stiglob.cxx
@@ -1,3 +1,11 @@
+#include "RVersion.h"
+
+#if ROOT_VERSION_CODE >= ROOT_VERSION(6,0,0)
+#include "TChain.h"
+extern constexpr long long int TChain::kBigNumber;
+#endif
+
+
 class StSstDbMaker;
 class StSsdDbMaker;
 class StSvtDbMaker;
